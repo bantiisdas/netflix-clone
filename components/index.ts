bc2c9e46e1ef46be0ccb63225e383f9a170daea3
Supriya Das <bantibuto3@gmail.com>
2023-07-17 19:12:16 +0530

    first commit

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import Banner from "./Banner";
+import Row from "./Row";
+import Thumbnail from "./Thumbnail";
+
+export {
+    Header,
+    Banner,
+    Row,
+    Thumbnail,
+}
